63207d21d207af52ebad41dcf753752c506dfab2
胡争辉 <hu@daonao.com>
2021-12-17 18:58:55 +0800

    add ratelimit-take

---
+++ ratelimit-take/magefile.go
@@ -0,0 +1,20 @@
+//go:build mage
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"go.uber.org/ratelimit"
+)
+
+func Take() {
+	rl := ratelimit.New(100)
+	prev := time.Now()
+	for i := 0; i < 10; i++ {
+		now := rl.Take()
+		fmt.Println(i, now.Sub(prev))
+		prev = now
+	}
+}
